899c2bd5e84d0ede6f05b95755552d193a61fe14
gudao-maoni <42340239+gudao-maoni@users.noreply.github.com>
2018-08-14 11:10:53 +0800

    Add files via upload

---
+++ src/com/sict/entity/users.java
@@ -0,0 +1,34 @@
+package com.sict.test.entity;
+
+public class users {
+	
+	private  int id;
+	private String username;
+	private String sex;
+	private String age;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+}
